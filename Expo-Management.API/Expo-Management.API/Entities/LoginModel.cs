a5e279f7b34ccf52d4f3264f8a78426d0ccbdeb2
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-06-04 18:08:47 -0600

    Create the entities for the login/registration

---
+++ Expo-Management.API/Expo-Management.API/Entities/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expo_Management.API.Entities
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string? Username { get; set; }
+        
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}
---
+++ Expo-Management.API/Expo-Management.API/Entities/RegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expo_Management.API.Entities
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string? Lastname { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}

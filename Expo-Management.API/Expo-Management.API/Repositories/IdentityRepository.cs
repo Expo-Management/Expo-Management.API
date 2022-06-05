c0d29b1daeda33c23cfbb9e37c00e8d41de7f655
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-06-04 18:10:40 -0600

    Create API for the expo management project.

---
+++ Expo-Management.API/Expo-Management.API/Repositories/IdentityRepository.cs
@@ -0,0 +1,85 @@
+﻿using Expo_Management.API.Entities;
+using Expo_Management.API.Interfaces;
+using Expo_Management.API.Utils;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Expo_Management.API.Repositories
+{
+    public class IdentityRepository : IIdentityRepository
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        private AuthUtils _authUtils;
+
+        public IdentityRepository(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            _authUtils = new AuthUtils(_userManager, _roleManager, _configuration);
+        }
+
+        public async Task<Response> RegisterNewUser(string Role, RegisterModel model)
+        {
+            var userExists = await _userManager.FindByNameAsync(model.Username);
+            if (userExists != null)
+                return new Response { Status = "Error", Message = "User already exists!" };
+
+            IdentityUser user = new()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." };
+
+            await _authUtils.AssignRole (user, Role);
+
+            return new Response { Status = "Success", Message = "User created successfully!" };
+        }
+
+        public async Task<LoginResponse?> LoginUser(LoginModel model)
+        {
+            var userRoleStored = "";
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                Console.WriteLine(userRoles);
+
+                foreach (var userRole in userRoles)
+                {
+                    Console.WriteLine(userRole);
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                    userRoleStored = userRole;
+                }
+
+                var token = _authUtils.GetToken(authClaims);
+
+                return new LoginResponse {
+                    Token = new JwtSecurityTokenHandler().WriteToken(token),
+                    Expiration = token.ValidTo,
+                    Role = userRoleStored
+                };
+            }
+            return null;
+        }
+    }
+}

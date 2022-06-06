23a0d6d4a7a86d3dd2495fb8b68ce4c56762ec33
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-06-06 11:08:54 -0600

    Create the model for the update of the users.

---
+++ Expo-Management.API/Expo-Management.API/Entities/UpdateUser.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expo_Management.API.Entities
+{
+    public class UpdateUser
+    {
+        [Required(ErrorMessage = "UserID is required")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        public string Phone { get; set; }
+    }
+}

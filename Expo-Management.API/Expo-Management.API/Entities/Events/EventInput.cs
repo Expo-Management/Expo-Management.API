f7c8f415a90914349730edf57eea7cd6e8ac1b58
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-06-09 22:47:35 -0600

    Create the entities for the event model, this for the create, update and database model-

---
+++ Expo-Management.API/Expo-Management.API/Entities/Events/EventInput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expo_Management.API.Entities.Events
+{
+    public class EventInput
+    {
+        public int? Id { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public string? Details { get; set; }
+        public int FairId { get; set; }
+    }
+}

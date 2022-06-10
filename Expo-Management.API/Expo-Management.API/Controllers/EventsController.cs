59f6677625da21a237cffc22cdff098e63a80fdc
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-06-09 22:48:33 -0600

    Create the event controller for the CRUD operations.

---
+++ Expo-Management.API/Expo-Management.API/Controllers/EventsController.cs
@@ -0,0 +1,99 @@
+﻿using Expo_Management.API.Entities.Events;
+using Expo_Management.API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expo_Management.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventsRepository _eventsRepository;
+
+        public EventsController(
+            IEventsRepository eventsRepository)
+        {
+            _eventsRepository = eventsRepository;
+        }
+
+        [HttpGet]
+        [Route("events")]
+        public async Task<IActionResult> GetEvents()
+        {
+            var response = await _eventsRepository.GetEventsAsync();
+
+            if (response == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(response);
+            }
+        }
+
+        [HttpGet]
+        [Route("event")]
+        public async Task<IActionResult> GetEvents(int EventId)
+        {
+            var response = await _eventsRepository.GetEventAsync(EventId);
+
+            if (response == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(response);
+            }
+        }
+
+        [HttpPost]
+        [Route("event")]
+        public async Task<IActionResult> CreateEvent([FromBody] EventInput model)
+        {
+            var response = await _eventsRepository.CreateEventAsync(model);
+
+            if (response == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok("Event created successfully!");
+            }
+        }
+
+        [HttpPut]
+        [Route("event")]
+        public async Task<IActionResult> UpdateEvent([FromBody] EventUpdate model)
+        {
+            var response = await _eventsRepository.UpdateEventAsync(model);
+
+            if (response == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok("Event updated successfully!");
+            }
+        }
+
+        [HttpDelete]
+        [Route("event")]
+        public async Task<IActionResult> DeleteEvent(int EventId)
+        {
+            var response = await _eventsRepository.DeleteEventAsync(EventId);
+
+            if (response)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok("Event removed successfully");
+            }
+        }
+    }
+}

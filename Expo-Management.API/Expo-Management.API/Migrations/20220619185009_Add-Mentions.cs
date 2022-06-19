5c988622dc73e1a58a0c593ff38038708fe102e9
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-06-19 14:12:33 -0600

    New migrations for update the database model

---
+++ Expo-Management.API/Expo-Management.API/Migrations/20220619185009_Add-Mentions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expo_Management.API.Migrations
+{
+    public partial class AddMentions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mention",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mention", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MentionProjectModel",
+                columns: table => new
+                {
+                    MentionsId = table.Column<int>(type: "int", nullable: false),
+                    ProjectsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MentionProjectModel", x => new { x.MentionsId, x.ProjectsId });
+                    table.ForeignKey(
+                        name: "FK_MentionProjectModel_Mention_MentionsId",
+                        column: x => x.MentionsId,
+                        principalTable: "Mention",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MentionProjectModel_Projects_ProjectsId",
+                        column: x => x.ProjectsId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MentionProjectModel_ProjectsId",
+                table: "MentionProjectModel",
+                column: "ProjectsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MentionProjectModel");
+
+            migrationBuilder.DropTable(
+                name: "Mention");
+        }
+    }
+}
---
+++ Expo-Management.API/Expo-Management.API/Migrations/20220619191723_Update-Project-Nulls.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expo_Management.API.Migrations
+{
+    public partial class UpdateProjectNulls : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Files_FilesId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FilesId",
+                table: "Projects",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Files_FilesId",
+                table: "Projects",
+                column: "FilesId",
+                principalTable: "Files",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Files_FilesId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FilesId",
+                table: "Projects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Files_FilesId",
+                table: "Projects",
+                column: "FilesId",
+                principalTable: "Files",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

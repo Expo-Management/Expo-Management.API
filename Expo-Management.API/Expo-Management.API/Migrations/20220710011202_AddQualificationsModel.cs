ddf5a4bffcdf61c99fed5a75e0cbd33ba35772f2
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-07-10 17:03:42 -0600

    Create migrations for new models and designs of the DB.

---
+++ Expo-Management.API/Expo-Management.API/Migrations/20220710011202_AddQualificationsModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expo_Management.API.Migrations
+{
+    public partial class AddQualificationsModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Qualifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Calification = table.Column<int>(type: "int", nullable: false),
+                    JudgeId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Qualifications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Qualifications_AspNetUsers_JudgeId",
+                        column: x => x.JudgeId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Qualifications_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Qualifications_JudgeId",
+                table: "Qualifications",
+                column: "JudgeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Qualifications_ProjectId",
+                table: "Qualifications",
+                column: "ProjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Qualifications");
+        }
+    }
+}

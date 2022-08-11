cb31480c684eca6cd974e0b28967ab150a777c28
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-08-10 22:20:38 -0600

    Create the view model to retrieve the needed information to qualify the project.

---
+++ Expo-Management.API/Expo-Management.API/Entities/Projects/QualifyProject.cs
@@ -0,0 +1,11 @@
+﻿namespace Expo_Management.API.Entities.Projects
+{
+    public class QualifyProject
+    {
+        public int Punctuation { get; set; }
+        public string Comments { get; set; }
+        public int ProjectId { get; set; }
+        public string JudgeEmail { get; set; }
+
+    }
+}

dfc83686a7d7b5b59f80a669c116aa74f8eef205
PC-ANDRES\andre <andreslml.ab@gmail.com>
2022-10-19 15:19:44 -0600

    Remove "News" migrations

---
+++ Expo-Management.API/Expo-Management.API.Infraestructure/Migrations/20221019210447_removeNews.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expo_Management.API.Infraestructure.Migrations
+{
+    public partial class removeNews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "New");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "New",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FairId = table.Column<int>(type: "int", nullable: false),
+                    PublisherId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_New", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_New_AspNetUsers_PublisherId",
+                        column: x => x.PublisherId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_New_Fair_FairId",
+                        column: x => x.FairId,
+                        principalTable: "Fair",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_New_FairId",
+                table: "New",
+                column: "FairId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_New_PublisherId",
+                table: "New",
+                column: "PublisherId");
+        }
+    }
+}
